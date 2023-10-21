d274baac60883e8a282e8273bad13eebb6381ab1
Oisín Thomas Morrin <oisin.thomas.morrin@gmail.com>
2023-10-21 13:17:22 +0100

    fix type issue in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -40,11 +40,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout(children) {
   return (
     <html lang="en">
       <head>
