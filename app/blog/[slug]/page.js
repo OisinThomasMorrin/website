612a8070e21f547adcefc5906754a83fbb2d6276
Oisín Thomas Morrin <oisin.thomas.morrin@gmail.com>
2023-10-21 13:04:47 +0100

    Fix site metadata import

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,7 +1,7 @@
 import BlogDetails from "@/components/Blog/BlogDetails";
 import RenderMdx from "@/components/Blog/RenderMdx";
 import Tag from "@/components/Elements/Tag";
-import siteMetadata from "@/../utils/siteMetaData";
+import siteMetadata from "@/utils/SiteMetaData";
 import { allBlogs } from "@/.contentlayer/generated";
 import { slug } from "github-slugger";
 
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
-const siteMetadata = require("./src/utils/siteMetaData");
+const siteMetadata = require("./utils/SiteMetaData");
 
 module.exports = {
   siteUrl: siteMetadata.siteUrl,
